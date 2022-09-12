b933b275eff9b97efc7bee63e37b8265a9d2073e
lewisaandres <lewisalipioandres@gmail.com>
2022-09-12 16:22:53 -0700

    Initial commit

---
+++ Bubblesort-AJ.py
@@ -0,0 +1,82 @@
+'''
+name1: AJ1 Fahim1
+name2: AJ2 Fahim2
+
+assignment:PA2
+'''
+import sys
+import random
+import time
+
+class Solution:
+	
+	#This function returns a descending sorted array.
+	def function_a (self, elements_count: int) -> list:
+		output = []
+		for i in range(elements_count,0, -1):
+			output.append(i)
+		return output
+
+	#This function returns an ascending sorted array.	
+	def function_b (self, elements_count: int) -> list:
+		output = []
+		for i in range(1, elements_count+1):
+			output.append(i)
+		return output
+
+	def function_c(self, elements_count: int, seed: int):
+		output = []
+		random.seed(seed)
+		for i in range(0,elements_count+1):
+			output.append(random.randint(1,1000000))
+
+		return output
+
+
+	def select_input(self, input_type: str, elements_count: int, seed: int) -> list:
+		output = []
+		if input_type == "a":
+			output = self.function_a(elements_count)
+		if 	input_type == "b":
+			output = self.function_b(elements_count)
+		if 	input_type == "c":
+			output = self.function_c(elements_count, seed)
+		return output	
+
+	def pa2_bubblesort (self, input_type: str, elements_count: int, seed: int) -> list:
+		output = []
+		query_list = self.select_input(input_type, elements_count, seed)
+		
+		n = len(query_list)
+
+		# get the start time
+		st = time.process_time()
+		
+    	# Traverse through all array elements
+		for i in range(n-1):
+        	# range(n) also work but outer loop will
+        	# repeat one time more than needed.
+        	# Last i elements are already in place
+			for j in range(0, n-i-1):
+ 
+            	# traverse the array from 0 to n-i-1
+				if query_list[j] > query_list[j + 1]:
+					query_list[j], query_list[j + 1] = query_list[j + 1], query_list[j]
+
+		et = time.process_time()
+		res = et - st
+
+		return [query_list, res]
+
+
+
+
+if __name__ == '__main__':
+	input_type = sys.argv[1]
+	elements_count = int(sys.argv[2])
+	seed = sys.argv[3]
+	
+	obj = Solution()
+	ret = obj.pa2_bubblesort(input_type, elements_count, seed)
+	print(ret)
+
---
+++ quicksort-yourname.py
@@ -0,0 +1,75 @@
+'''
+name1: AJ1 Fahim1
+name2: AJ2 Fahim2
+
+assignment:PA2
+'''
+import sys
+import random
+import time
+
+class Solution:
+	
+	#This function returns a descending sorted array.
+	def function_a (self, elements_count: int) -> list:
+		output = []
+		for i in range(elements_count,0, -1):
+			output.append(i)
+		return output
+
+	#This function returns an ascending sorted array.	
+	def function_b (self, elements_count: int) -> list:
+		output = []
+		for i in range(1, elements_count):
+			output.append(i)
+		return output
+
+	def function_c(self, elements_count: int, seed: int):
+		output = []
+		random.seed(seed)
+		for i in range(0,elements_count+1):
+			output.append(random.randint(1,1000000))
+
+		return output
+
+
+	def select_input(self, input_type: str, elements_count: int, seed: int) -> list:
+		output = []
+		if input_type == "a":
+			output = self.function_a(elements_count)
+		if 	input_type == "b":
+			output = self.function_b(elements_count)
+		if 	input_type == "c":
+			output = self.function_c(elements_count, seed)
+		return output	
+
+	def pa2_bubblesort (self, input_type: str, elements_count: int, seed: int) -> list:
+		output = []
+		query_list = self.select_input(input_type, elements_count, seed)
+		
+		n = len(query_list)
+
+		# get the start time
+		st = time.process_time()
+		
+    	# your quicksort algorithm comes here ...
+
+    	# end of quicksort
+		
+		et = time.process_time()
+		res = et - st
+
+		return [query_list, res]
+
+
+
+
+if __name__ == '__main__':
+	input_type = sys.argv[1]
+	elements_count = int(sys.argv[2])
+	seed = sys.argv[3]
+	
+	obj = Solution()
+	ret = obj.pa2_quicksort(input_type, elements_count, seed)
+	print(ret)
+
